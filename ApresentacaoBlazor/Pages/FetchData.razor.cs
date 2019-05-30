9425280b7d6404a4a67d3e1471717450857328de
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-05-30 11:15:56 -0300

    Components genéricos, code behind

---
+++ ApresentacaoBlazor/Pages/FetchData.razor.cs
@@ -0,0 +1,21 @@
+﻿using ApresentacaoBlazor.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApresentacaoBlazor.Pages
+{
+    public class FetchDataComponent : ComponentBase
+    {
+        [Inject] protected HttpClient Http { get; set; }
+
+        protected WeatherForecast[] Forecasts { get; set; }
+
+        protected override async Task OnInitAsync()
+        {
+            Forecasts = await Http.GetJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+            Console.WriteLine("Testes");
+        }
+    }
+}
